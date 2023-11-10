82685e3400d800bedbb96db1a185f4609c95b04f
MiriamLaw <mimsical999@yahoo.com>
2023-11-09 20:27:52 -0800

    Working and needs work

--- src/main/java/com/coderscampus/assignment/DataFetcher.java
+++ src/main/java/com/coderscampus/assignment/DataFetcher.java
@@ -5,17 +5,20 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class DataFetcher {
 
 	public static void main(String[] args) {
-int numberOfChunks = 1000;
+		int numberOfChunks = 1000;
 		
 		Assignment8 assignment8 = new Assignment8();
+		Map<Integer, AtomicInteger> counts = new ConcurrentHashMap<>();
 		ExecutorService exService = Executors.newCachedThreadPool();
 		
 		List<CompletableFuture<List<Integer>>> futures = new ArrayList<>();
@@ -24,53 +27,58 @@ int numberOfChunks = 1000;
 			final int chunk = i;
 			
 			CompletableFuture<List<Integer>> future = CompletableFuture.supplyAsync(() -> 
-												fetchDataChunk(chunk, assignment8, exService), exService).exceptionally(ex -> {
+												fetchDataChunk(chunk, assignment8, counts, exService), exService).exceptionally(ex -> {
 													System.err.println("Exception occurred: " + ex.getMessage());
 													return new ArrayList<>();
 												});
 			futures.add(future);
 		}
-		//combine into one completablefuture when done
+
 		CompletableFuture<Void> all = CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
 		
+		all.thenRun(() -> {
+//			System.out.println("Counts before printing: " + counts);
+			printTotalCounts(counts);
+			
+		});
 		
-		//ensure program doesn't terminate immediately
 		all.join();
-		
-		List<Integer> results = futures.stream()
-										.map(CompletableFuture::join)
-										.flatMap(List::stream)
-										.collect(Collectors.toList());
-		
-		calculateFrequency(results);
-		
+		System.out.println("Processing completed!");
+
 		exService.shutdown();
-		
+
 	}
-	private static void calculateFrequency(List<Integer> results) {
-		System.out.println("calculateFrequency method called with results: " + results);
-		Map<Integer, Integer> frequencyMap = new HashMap<>();
-		
-		for (Integer number : results) {
-			frequencyMap.put(number, frequencyMap.getOrDefault(number, 0) + 1);
-		}
-		
-		for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
-			System.out.println(entry.getKey() + "=" + entry.getValue());
+
+	private static void printTotalCounts(Map<Integer, AtomicInteger> counts) {
+		synchronized (counts) {
+			
+			StringBuilder result = new StringBuilder();
+
+			for (int i = 1; i <= 14; i++) {
+
+				int count = counts.getOrDefault(i, new AtomicInteger(0)).get();
+				result.append(i).append("=").append(count).append(", ");
+//				System.out.println(i + "=" + count);
+			}
+			
+			if (result.length() > 0) {
+				result.setLength(result.length());
+			}
+			System.out.println(result);
 		}
 	}
-	private static List<Integer> fetchDataChunk(int chunk, Assignment8 assignment8, ExecutorService exService) {
+	private static List<Integer> fetchDataChunk(int chunk, Assignment8 assignment8, Map<Integer, AtomicInteger> counts, ExecutorService exService) {
 		int chunkSize = 1000;
 		int start = chunk * chunkSize;
 		int end = start + chunkSize;
 		
-		System.out.println("Processing chunk: " + chunk + " from " + start + " to " + end);
+//		System.out.println("Processing chunk: " + chunk + " from " + start + " to " + end);
 		
 		List<Integer> dataChunk = IntStream.range(start, end)
 				.boxed()
 				.map(n -> assignment8.getNumbers(n, n+1))
 				.flatMap(List::stream)
-				.parallel()
+//				.parallel()
 				.limit(10)
 				.map(i -> {
 					
@@ -79,6 +87,7 @@ int numberOfChunks = 1000;
 					} catch (InterruptedException e) {
 						Thread.currentThread().interrupt();
 					}
+					counts.computeIfAbsent(i, k -> new AtomicInteger()).incrementAndGet();
 					return i;
 				})
 				.collect(Collectors.toList());
